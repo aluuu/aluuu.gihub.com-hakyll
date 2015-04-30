f3685af870e7e2e0be654df1291c1668fa8de5b2
Alexander Dinu <aluuu@husa.su>
2015-04-30 15:14:10 +0500

    yay! there's working multilingual setup now

--- site.hs
+++ site.hs
@@ -2,53 +2,60 @@
 import Data.Monoid (mappend)
 import Hakyll
 
-main :: IO ()
-main = hakyll $ do
-    match "images/*" $ do
-        route   idRoute
-        compile copyFileCompiler
+matchPosts lang =
+    match pattern $ do
+      route $ setExtension "html"
+      compile $ pandocCompiler
+                  >>= loadAndApplyTemplate "templates/post.html"    postCtx
+                  >>= loadAndApplyTemplate "templates/default.html" postCtx
+                  >>= relativizeUrls
+    where pattern = fromGlob ("posts/" ++ lang ++ "/*")
 
-    match "css/*" $ do
-        route   idRoute
-        compile compressCssCompiler
-
-    match "posts/*" $ do
-        route $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
-            >>= relativizeUrls
-
-    create ["archive.html"] $ do
+matchArchive lang =
+    create [pattern] $ do
         route idRoute
         compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
+            posts <- recentFirst =<< loadAll postsPattern
             let archiveCtx =
                     listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Archives"            `mappend`
+                    constField "title" "Archives" `mappend`
                     defaultContext
 
             makeItem ""
                 >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
                 >>= loadAndApplyTemplate "templates/default.html" archiveCtx
                 >>= relativizeUrls
+    where pattern = fromFilePath ("archive_" ++ lang ++ ".html")
+          postsPattern = fromGlob ("posts/" ++ lang ++ "/*")
+
+main :: IO ()
+main = hakyll $ do
+    match "images/*" $ do
+        route idRoute
+        compile copyFileCompiler
+
+    match "css/*" $ do
+        route idRoute
+        compile compressCssCompiler
+
+    matchPosts "ru"
+    matchPosts "en"
+
+    matchArchive "ru"
+    matchArchive "en"
 
     match "index.html" $ do
         route idRoute
         compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
             let indexCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Home"                `mappend`
+                    constField "title" "Home" `mappend`
                     defaultContext
-
             getResourceBody
                 >>= applyAsTemplate indexCtx
                 >>= loadAndApplyTemplate "templates/default.html" indexCtx
                 >>= relativizeUrls
 
     match "templates/*" $ compile templateCompiler
-
 
 postCtx :: Context String
 postCtx =
