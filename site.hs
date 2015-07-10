9b1db685903db1a85c793681931849c62c644ad1
Alexander Dinu <aluuu@husa.su>
2015-07-10 13:51:21 +0500

    prettify site's appearance

--- site.hs
+++ site.hs
@@ -2,60 +2,37 @@
 import Data.Monoid (mappend)
 import Hakyll
 
-lastNPosts n pattern=
-    fmap (take n) $ loadAll pattern >>= recentFirst
-
-matchPosts lang =
-    match pattern $ do
-      route $ setExtension "html"
-      compile $ pandocCompiler
-                  >>= loadAndApplyTemplate "templates/post.html"    postCtx
-                  >>= loadAndApplyTemplate "templates/default.html" postCtx
-                  >>= relativizeUrls
-    where pattern = fromGlob ("posts/" ++ lang ++ "/*")
-
-matchArchive lang =
-    create [pattern] $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll postsPattern
-            let archiveCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Archives" `mappend`
-                    defaultContext
-
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-                >>= relativizeUrls
-    where pattern = fromFilePath ("archive_" ++ lang ++ ".html")
-          postsPattern = fromGlob ("posts/" ++ lang ++ "/*")
+postCtx :: Context String
+postCtx =
+    dateField "date" "%d.%m.%Y" `mappend`
+    defaultContext
 
 main :: IO ()
 main = hakyll $ do
-    match "images/*" $ do
-        route idRoute
-        compile copyFileCompiler
+    match "images/*" $
+          do
+            route idRoute
+            compile copyFileCompiler
 
-    match "css/*" $ do
-        route idRoute
-        compile compressCssCompiler
+    match "css/*" $
+          do
+            route idRoute
+            compile compressCssCompiler
 
-    matchPosts "ru"
-    matchPosts "en"
+    match "posts/ru/*" $
+          do
+            route $ setExtension "html"
+            compile $ pandocCompiler
+                >>= loadAndApplyTemplate "templates/post.html"    postCtx
+                >>= loadAndApplyTemplate "templates/default.html" postCtx
+                >>= relativizeUrls
 
-    matchArchive "ru"
-    matchArchive "en"
-
-    match "index.html" $ do
-        route idRoute
+    match "templates/index.html" $ do
+        route $ constRoute "index.html"
         compile $ do
-            ru_posts <- lastNPosts 5 "posts/ru/*"
-            en_posts <- lastNPosts 5 "posts/en/*"
+            posts <- loadAll "posts/ru/*" >>= recentFirst
             let indexCtx =
-                    listField "ru_posts" postCtx (return ru_posts) `mappend`
-                    listField "en_posts" postCtx (return en_posts) `mappend`
-                    constField "title" "Home" `mappend`
+                    listField "posts" postCtx (return posts) `mappend`
                     defaultContext
             getResourceBody
                 >>= applyAsTemplate indexCtx
@@ -63,8 +40,3 @@ main = hakyll $ do
                 >>= relativizeUrls
 
     match "templates/*" $ compile templateCompiler
-
-postCtx :: Context String
-postCtx =
-    dateField "date" "%d.%m.%Y" `mappend`
-    defaultContext
